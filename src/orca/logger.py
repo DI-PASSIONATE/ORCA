3bd9b7b442dfd6f77bd47f47a40460e642a63a41
David Lurz <david.lurz@t-online.de>
2026-01-07 00:59:42 +0100

    custom logger

---
+++ src/orca/logger.py
@@ -0,0 +1,17 @@
+import logging
+import colorlog
+import sys
+
+logger = logging.getLogger("ORCA")
+
+stdout = colorlog.StreamHandler(stream=sys.stdout)
+
+fmt = colorlog.ColoredFormatter(
+    fmt="%(name)s: %(white)s%(asctime)s%(reset)s | %(log_color)s%(levelname)s%(reset)s >> %(log_color)s%(message)s%(reset)s",
+    datefmt="%H:%M:%S"
+)
+
+stdout.setFormatter(fmt)
+logger.addHandler(stdout)
+
+logger.setLevel(logging.DEBUG)
