d25fd665d2793747a387412e46f7f53b6651f0dc
David Lurz <david.lurz@t-online.de>
2026-02-03 23:29:05 +0100

    geo_to_s_param dataset fix for new API

--- src/orca/training/datasets/geo_to_s_param.py
+++ src/orca/training/datasets/geo_to_s_param.py
@@ -2,6 +2,7 @@ import pandas as pd
 import skrf as rf
 import os
 import numpy as np
+import torch
 
 from orca.training.datasets.base_dataset import BaseDataset
 from orca.training.normalize import StandardNormalizer
@@ -14,14 +15,10 @@ class GeoToSParamDataset(BaseDataset):
     a training sample for each frequency point in the S-parameter data. Each sample consists
     of input parameters and corresponding S-parameter values.
     """
-    def __init__(self, data_dir: str, split: str = "all", n_ports=6, features=None, input_normalizer=None, output_normalizer=None):
-        super(GeoToSParamDataset, self).__init__(data_dir, split, features, input_normalizer, output_normalizer)
+    def __init__(self, n_ports=6, features=None, input_normalizer=None, output_normalizer=None):
+        super(GeoToSParamDataset, self).__init__(features, input_normalizer, output_normalizer)
 
-        # Load parameters from CSV
-        self.params_df = pd.read_csv(os.path.join(data_dir, "params.csv"))
-
-        self.input_param_names = list(self.params_df.columns)
-        self.input_param_names.remove('name')  # Remove 'name' column
+        self.n_ports = n_ports
 
         # Initialize output parameter names
         self.output_param_names = [
@@ -30,30 +27,21 @@ class GeoToSParamDataset(BaseDataset):
             for j in range(n_ports)
             for part in ("real", "imag")
         ]
-    
-        for idx, row in self.params_df.iterrows():
-            geometry_name = row['name'] # = name.s4p
-            s4p_path = f"{data_dir}/{geometry_name}_dc_deembedded.s{n_ports}p"
 
-            if not os.path.exists(s4p_path):
-                logger.error(f"S-parameter file not found: {s4p_path}")
+    def load_samples(self, directory: str, data_df: pd.DataFrame) -> None:
+        for idx, row in data_df.iterrows():
+            geometry_name = row['name'] # = name.s4p
+            snp_path = f"{directory}/{geometry_name}_dc_deembedded.s{self.n_ports}p"
+
+            if not os.path.exists(snp_path):
+                logger.error(f"S-parameter file not found: {snp_path}")
                 continue
 
             geometry_params = np.array(row.drop('name'), dtype=np.float32)
-            sample = self.load_samples(s4p_path, geometry_params)
-
-            rand_num = self.random.rand()
-            if \
-            (self.split == "all") or \
-            (self.split == "train" and rand_num < 0.7) or \
-            (self.split == "val" and 0.7 <= rand_num < 0.85) or \
-            (self.split == "test" and rand_num >= 0.85):
-                self.samples.append(sample)
-
-        if self.output_normalizer is not None:
-            self.output_normalizer.set_samples([y for _, y in self.samples])
-
-    def load_samples(self, sparam_path: str, geometry_params: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+            sample = self.load_single_sample(snp_path, geometry_params)
+            self.samples.append(sample)
+    
+    def load_single_sample(self, sparam_path: str, geometry_params: np.ndarray) -> tuple[torch.Tensor, torch.Tensor]:
         """Load S-parameter data from a Touchstone file and return a single sample with all frequencies."""
         
         net = rf.Network(sparam_path)
@@ -74,5 +62,7 @@ class GeoToSParamDataset(BaseDataset):
 
         # Input is only geometry parameters (no frequency)
         x = geometry_params.astype(np.float32)
+
+        x, y = torch.tensor(x, dtype=torch.float32, device=self.device), torch.tensor(y, dtype=torch.float32, device=self.device)
 
         return (x, y)
