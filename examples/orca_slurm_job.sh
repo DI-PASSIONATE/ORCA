d8c5bf3ba390da92c48644342aa3f5e6f8b86985
David Lurz <david.lurz@t-online.de>
2026-08-01 02:07:27 +0200

    updated slurm batch script

--- examples/orca_slurm_job.sh
+++ examples/orca_slurm_job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -l
 #
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=72
-#SBATCH --time=00:30:00
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=36
+#SBATCH --time=01:00:00
 #SBATCH --job-name=ORCA-FEM-SIM
 #SBATCH --export=NONE
 
@@ -13,6 +14,16 @@ unset SLURM_EXPORT_ENV
 module load python
 module load intel/2025.2.0
 module load openmpi/5.0.8-intel2025.2.0
+
+# Set the number of threads for OpenMP
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# for Slurm version >22.05: cpus-per-task has to be set again for srun
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+
+# Pin OpenMP threads to cores and close them together to avoid thread migration
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
 
 # Activate the conda environment
 conda activate orca
