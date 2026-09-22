c345d5a1efb8aac35c09523560df82b459780592
David Lurz <david.lurz@t-online.de>
2026-09-22 12:02:00 +0200

    ruff fixes

--- examples/main.py
+++ examples/main.py
@@ -1,5 +1,6 @@
 import orca
-from orca.geometry.presets.tf_octa_c_ports import TransformerOcta
+
+# from orca.geometry.presets.tf_octa_c_ports import TransformerOcta
 from orca.geometry.presets.inductor_octa import InductorOcta
 
 PLOT = False
